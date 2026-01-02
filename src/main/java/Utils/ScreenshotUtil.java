d10d98e316ee4d6dcea56bf380355c2445d67839
Digmy <digmy25@gmail.com>
2026-01-02 12:51:05 +0100

    segunda pasada 02012026

---
+++ src/main/java/Utils/ScreenshotUtil.java
@@ -0,0 +1,72 @@
+package Utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import Driver.Datadriver_Factory;
+
+//la llamo desde un TestNG Listener cuando un test falla
+public class ScreenshotUtil {
+
+    //es una clase utilitaria que no requiere de instanciacion por eso private
+    private ScreenshotUtil() {}
+
+    //mejor que out.println
+    private static final Logger log = LogManager.getLogger(ScreenshotUtil.class);
+
+    //uso final porque es una constante
+    public static final String takeScreenshot(String testName) {
+
+        //refuerza el uso correcto del TheardLocal, ya que obtengo el driver correcto en cada caso
+        WebDriver driver = Datadriver_Factory.getDriver();
+
+        //prevuene NullPointerException(control defensivo)
+        if (driver == null) {
+            log.error("No WebDriver instance found. Screenshot aborted.");
+               return null;
+        }
+
+        try {
+            // Crear nombre único
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String fileName = testName + "_" + timestamp + ".png";
+
+            // Crear carpeta si no existe
+            String directoryPath = "screenshots/fails/";
+            File directory = new File(directoryPath);
+            if (!directory.exists()) directory.mkdirs();
+
+            // Definir ruta final
+            String filePath = directoryPath + fileName;
+
+            // Tomar screenshot
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File dest = new File(filePath);
+
+            // Guardarlo
+            Files.copy(src.toPath(), dest.toPath());
+
+            log.info("Screenshot saved at: " + dest.getAbsolutePath());
+            return dest.getAbsolutePath();
+
+        } catch (IOException e) {
+            log.error("Error saving screenshot: " + e.getMessage());
+            return null;
+        }
+    }
+}
+/*
+ * Toma captura de pantalla y la guarda en /screenshots/fails/
+ *
+ * @param testName nombre del test que falló
+ * @return ruta donde se guardó la imagen
+ */
--- src/main/java/Utils/Waits.java
+++ src/main/java/Utils/Waits.java
@@ -1,4 +1,80 @@
 package Utils;
 
+import Driver.Datadriver_Factory;
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+
+//evito duplicar WebDriverWait
 public class Waits {
+
+    private static int TIMEOUT_SECONDS = 10;
+    //ayuda a debuggear
+    private static final Logger logger = LogManager.getLogger(Waits.class);
+
+    // constructor privado en clase utilitaria
+    private Waits() {}
+
+    private static WebDriverWait getWait(){
+        return new WebDriverWait(
+                Datadriver_Factory.getDriver(),
+                Duration.ofSeconds(TIMEOUT_SECONDS)
+        );
+
+    }
+    //espera a que el elemento sea visible por localizador
+    public static WebElement waitForVisibility(By locator){
+        logger.info("Esperando visibilidad del elemento: " + locator);
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //espera a que el elemento sea visible por elemento
+    public static WebElement waitForVisibilityOfElement(WebElement element){
+        logger.info("Esperando visibilidad del elemento WebElement");
+        return getWait().until(ExpectedConditions.visibilityOf(element));
+    }
+
+    //espera a que los elementos sean visibles por localizador
+    public static List<WebElement> waitForVisibilityOfElements(By locator){
+        logger.info("Esperando visibilidad del elemento: " + locator);
+        return getWait().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+    }
+
+    //espera a que los elementos sean visibles por lista de elementos
+    public static List<WebElement> waitForVisibilityOfAll(WebElement element){
+        logger.info("Esperando visibilidad del elemento WebElement");
+        return getWait().until(ExpectedConditions.visibilityOfAllElements(element));
+    }
+
+    //espera a que el elemento por su localizador sea clickable
+    public static WebElement waitForClickableByLocator(By locator){
+        logger.info("Esperando visibilidad del elemento: " + locator);
+        return getWait().until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    //espera a que el elemento sea clickable
+    public static WebElement waitForClickableByElement(WebElement element){
+        logger.info("Esperando visibilidad del elemento WebElement");
+        return getWait().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    //espera que el loader desaparezca
+    public static void waitUntilLoaderDisappear(By locator){
+        logger.info("Esperando visibilidad del elemento: " + locator);
+        getWait().until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
 }
+/*Qué decir en entrevista sobre esta clase (MEMORIZÁ)
+
+“Centralizo los waits explícitos en una clase utilitaria.
+Cada método crea su propio WebDriverWait usando el driver actual del ThreadLocal, lo que evita problemas en ejecución paralela.
+Así evito usar Thread.sleep y mantengo los Page Objects limpios.”*/
